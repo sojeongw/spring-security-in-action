5430ee408b0c990fe96556983448f1763ace2f3c
sojeongw <sojeongw.official@gmail.com>
2025-03-08 19:53:25 +0900

    init

---
+++ src/test/java/security/springsecurityinaction/SpringSecurityInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package security.springsecurityinaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityInActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
